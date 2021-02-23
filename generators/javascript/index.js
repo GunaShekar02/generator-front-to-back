3044ce17b899951515a3cc12d9db0fc8aa700edb
Guna Shekar <gunashekherproddatoori@gmail.com>
2021-02-23 22:39:49 +0530

    fix eslint errors

--- generators/javascript/index.js
+++ generators/javascript/index.js
@@ -74,11 +74,10 @@ module.exports = class extends Generator {
       }
     ];
 
-    frontendPrompts.forEach(
-      prompt =>
-        (prompt.when = answers =>
-          answers.type === "frontend" || answers.type === "fullstack")
-    );
+    frontendPrompts.forEach(prompt => {
+      prompt.when = answers =>
+        answers.type === "frontend" || answers.type === "fullstack";
+    });
 
     const backendPrompts = [
       {
@@ -141,11 +140,10 @@ module.exports = class extends Generator {
       }
     ];
 
-    backendPrompts.forEach(
-      prompt =>
-        (prompt.when = answers =>
-          answers.type === "backend" || answers.type === "fullstack")
-    );
+    backendPrompts.forEach(prompt => {
+      prompt.when = answers =>
+        answers.type === "backend" || answers.type === "fullstack";
+    });
 
     if (!this.options.appname) {
       initialPrompts.unshift({
@@ -155,7 +153,7 @@ module.exports = class extends Generator {
       });
     }
 
-    return await this.prompt([
+    const results = await this.prompt([
       ...initialPrompts,
       ...frontendPrompts,
       ...backendPrompts
@@ -174,6 +172,8 @@ module.exports = class extends Generator {
       this.linter = r.linter;
       this.specification = r.specification;
     });
+
+    return results;
   }
 
   writing() {
@@ -194,6 +194,7 @@ module.exports = class extends Generator {
           src + "/src/Redux/Reducers/example.reducer.js"
         );
       }
+
       if (!this.axios) {
         copyOpts.globOptions.ignore.push(
           src + "/src/Services/example.service.js"
@@ -225,6 +226,7 @@ module.exports = class extends Generator {
         );
       });
     }
+
     if (this.type === "fullstack" || this.type === "backend") {
       const src = this.sourceRoot() + "/backend/**";
       const dest = this.destinationPath(`${this.name}/backend`);
@@ -232,6 +234,7 @@ module.exports = class extends Generator {
         "package.json",
         "README.md",
         ".env",
+        "docker-compose.yml",
         ".eslintrc.json",
         "server/routes.js",
         "test/examples.controller.js",
@@ -258,9 +261,11 @@ module.exports = class extends Generator {
         files.push("server/common/api.v2.yml");
         copyOpts.globOptions.ignore.push(src + "/server/common/api.yml");
       }
+
       if (!this.docker) {
         copyOpts.globOptions.ignore.push(src + "/+(Dockerfile|.dockerignore)");
       }
+
       if (this.authentication === "none") {
         copyOpts.globOptions.ignore.push(
           src + "/server/api/middlewares/isAuthenticated.session.js"
@@ -339,9 +344,9 @@ module.exports = class extends Generator {
   }
 
   install() {
-    this.log(chalk.blue("Installing backend dependencies..."));
-    let appDir;
     if (this.type === "backend" || this.type === "fullstack") {
+      this.log(chalk.blue("Installing backend dependencies..."));
+      let appDir;
       appDir = path.join(process.cwd(), `${this.name}/backend`);
       process.chdir(appDir);
       if (this.useYarn) {
@@ -353,12 +358,12 @@ module.exports = class extends Generator {
   }
 
   end() {
-    this.log(chalk.blue("Installing frontend dependencies..."));
-    let appDir = process.cwd();
-    if (appDir.includes(this.name))
-      appDir = path.join(process.cwd(), `../frontend`);
-    else appDir = path.join(process.cwd(), `${this.name}/frontend`);
     if (this.type === "frontend" || this.type === "fullstack") {
+      this.log(chalk.blue("Installing frontend dependencies..."));
+      let appDir = process.cwd();
+      if (appDir.includes(this.name))
+        appDir = path.join(process.cwd(), `../frontend`);
+      else appDir = path.join(process.cwd(), `${this.name}/frontend`);
       process.chdir(appDir);
       if (this.useYarn) {
         this.yarnInstall();
