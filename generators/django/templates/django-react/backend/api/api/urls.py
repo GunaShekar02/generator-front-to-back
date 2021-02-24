5a2cee7cf6fb04f4d884f6b2e3f5c90512462f08
skeshavaa <s.keshavaa@gmail.com>
2021-02-24 16:27:18 -0500

    swagger now working

--- generators/django/templates/django-react/backend/api/api/urls.py
+++ generators/django/templates/django-react/backend/api/api/urls.py
@@ -38,7 +38,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/" ,include('blog.urls')),
-    path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path("swagger/", schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ] 
