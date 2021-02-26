aedcdae280cf31f2700d3afb76ddf11526504a7b
PayasSingh <payas@ualberta.ca>
2021-02-26 05:15:00 -0700

    fix urls.py

--- generators/django/templates/django/mysite/mysite/urls.py
+++ generators/django/templates/django/mysite/mysite/urls.py
@@ -39,9 +39,8 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapi.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
-    # <% if (swagger) { %>
-    # path("swagger/", schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    # <% } %>
+    <% if (swagger) { %>
+    path("swagger/", schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    <% } %>
 ]
 
