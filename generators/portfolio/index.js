a9bfa48a3dfcbf49b04f47463e5029a16154fa48
felixfaisal <faisalahmedfarooq46@gmail.com>
2021-04-15 22:22:58 +0530

    Updated index.js for portfolio

--- generators/portfolio/index.js
+++ generators/portfolio/index.js
@@ -5,7 +5,22 @@ const yosay = require("yosay");
 const path = require("path");
 
 module.exports = class extends Generator {
-  prompting() {
+  constructor(args, opts) {
+    super(args, opts);
+    this.argument("appname", { type: String, required: false });
+    this.option("yarn", {
+      description: "Use Yarn as the package manager"
+    });
+
+    this.useYarn = this.options.yarn;
+    this.name = "";
+    this.description = "My cool app";
+    this.version = "1.0.0";
+    this.apiRoot = "/api/v1";
+    this.specification = "openapi_3";
+  }
+
+  async prompting() {
     // Have Yeoman greet the user.
     this.log(
       yosay(
@@ -19,20 +34,23 @@ module.exports = class extends Generator {
       {
         type: "input",
         name: "name",
-        message: "App Name",
-        default: "myapp"
+        message: `App name [myapp]`
       }
     ];
 
     return this.prompt(prompts).then(props => {
+      // This.log(this.props)
+      // this.log(this.name)
+      // this.props.name = this.prompts.name || "myapp"
       // To access props later use this.props.someAnswer;
       this.props = props;
     });
   }
 
   writing() {
-    const src = this.sourceRoot();
-    const dest = this.destinationPath(`${this.props.name}`);
+    const file = this.props.name || "myapp";
+    const src = this.sourceRoot() + "/frontend/**";
+    const dest = this.destinationPath(`${file}/frontend`);
 
     // The ignore array is used to ignore files, push file names into this array that you want to ignore.
     const copyOpts = {
@@ -41,31 +59,32 @@ module.exports = class extends Generator {
       }
     };
 
-    if (!this.props.mongodb)
-      copyOpts.globOptions.ignore.push(src + "/mongoose.js");
+    // If (!this.props.mongodb)
+    //    copyOpts.globOptions.ignore.push(src + "/mongoose.js");
 
     this.fs.copy(src, dest, copyOpts);
 
-    const files = ["index.js", "package.json"];
+    /* Const files = ["index.js", "package.json"];
 
-    const opts = {
-      name: this.props.name,
-      mongodb: this.props.mongodb
-    };
+        const opts = {
+            name: this.props.name,
+            mongodb: this.props.mongodb
+        };
 
-    files.forEach(file => {
-      this.fs.copyTpl(
-        this.templatePath(file),
-        this.destinationPath(`${this.props.name}/${file}`),
-        opts,
-        copyOpts
-      );
-    });
+        files.forEach(file => {
+            this.fs.copyTpl(
+                this.templatePath(file),
+                this.destinationPath(`${this.props.name}/${file}`),
+                opts,
+                copyOpts
+            );
+        }); */
   }
 
   install() {
     const appDir = path.join(process.cwd(), this.props.name);
     process.chdir(appDir);
-    this.npmInstall();
+    // This.npmInstall();
+    this.log("Npm install");
   }
 };
